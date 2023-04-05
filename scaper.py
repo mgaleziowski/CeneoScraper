b4472a6791d7da0b966904be4210b55f84c3011e
mgaleziowski <mikizereq@gmail.com>
2023-04-05 12:07:34 +0200

    2023/04/05 MG

--- scaper.py
+++ scaper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #product_code=input("Podaj kod produktu: ")
-product_code="138551076"
+product_code="96875119"
 url=f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response=requests.get(url)
 page=BeautifulSoup(response.text,'html.parser')
