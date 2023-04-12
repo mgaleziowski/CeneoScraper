80fc7562dd166c3fc5d8f4025e64585f60e30b8f
mgaleziowski <mikizereq@gmail.com>
2023-04-12 12:41:13 +0200

    2023/04/12 MG

--- scaper.py
+++ scaper.py
@@ -1,40 +1,52 @@
-import requests 
+import requests, json 
 from bs4 import BeautifulSoup
 
-def get_some(ancestor, selector=None, attribute=None, return_list=False):
+def get_element(ancestor, selector=None, attribute=None, return_list=False):
        try:
         if return_list:
             return [tag.text.strip() for tag in ancestor.select(selector)].copy()
-        if not selector:
+        if not selector and attribute:
             return ancestor[attribute]
         if attribute:
             return ancestor.select_one(selector)[attribute].strip()
         return ancestor.select_one(selector).text.strip()
        except AttributeError:
            return None
+       
+selectors={
+        "opinion_id":[None,"data-entry-id"],
+        "author":["span.user-post__author-name"],
+        "recomendation":["span.user-post__author-recomendation > em"],
+        "score":["span.user-post__score-count"],
+        "purchased":["div.review-pz"],
+        "published_at":["span.user-post__published > time:nth-child(1)","datetime"],
+        "purchased_at":["span.user-post__published > time:nth-child(2)","datetime"],
+        "thubs_up":["button.vote-yes > span"],
+        "thumbs_down":["button.vote-no > span"],
+        "content":["div.user-post__text"],
+        "pros":["div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item",None,True],
+        "cons":["div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item",None,True],
+    }
 
 #product_code=input("Podaj kod produktu: ")
 product_code="96875119"
-url=f"https://www.ceneo.pl/{product_code}#tab=reviews"
-response=requests.get(url)
-page=BeautifulSoup(response.text,'html.parser')
-opinions=page.select("div.js_product-review")
-all_opinions=[]
-for opinion in opinions:
-    print(opinion["data-entry-id"])
-    single_opinion={
-        "opinion_id":opinion["data-entry-id"],
-        "author":opinion.select_one("span.user-post__author-name").text.strip(),
-        "recomendation":opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
-        "score":opinion.select_one("span.user-post__score-count").text.strip(),
-        "purchased":opinion.select_one("div.review-pz").text.strip(),
-        "published_at":opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
-        "purchased_at":opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
-        "thubs_up":opinion.select_one("button.vote-yes > span").text.strip(),
-        "thumbs_down":opinion.select_one("button.vote-no >span").text.strip(),
-        "content":opinion.select_one("div.user-post__text").text.strip(),
-        "pros":[pros.text.strip() for pros in opinion.select("div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item")],
-        "cons":[pros.text.strip() for pros in opinion.select("div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item")]  
-    }
-    all_opinions.append(single_opinion)
-    print(all_opinions)
+#product_code="11017453"
+nr=1
+while(nr<3):
+    url=f"https://www.ceneo.pl/{product_code}/opinie{nr}"
+    response=requests.get(url)
+    page=BeautifulSoup(response.text,'html.parser')
+    #print(get_element(page))
+    opinions=page.select("div.js_product-review")
+    all_opinions=[]
+    for opinion in opinions:
+        single_opinion={}
+        #print(opinion["data-entry-id"])
+        for k,v in selectors.items():
+            single_opinion[k]=get_element(opinion,*v)
+        all_opinions.append(single_opinion)
+        with open(f"./opinions/{product_code}.json","w",encoding="UTF-8") as jf:
+            json.dump(all_opinions,jf,indent=4,ensure_ascii=False)
+
+
+    
